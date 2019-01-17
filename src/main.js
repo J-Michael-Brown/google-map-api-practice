ba8f1961e232132a6147836df5cf36f6dc429bc1
Daniel Burbach and Michael Brown <michael@>
2019-01-17 14:48:18 -0800

    change from an .ajax use of API call to a promise call

--- src/main.js
+++ src/main.js
@@ -2,25 +2,58 @@ import './styles.css';
 import $ from 'jquery';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+let googleMapsClient = require('@google/maps').createClient({
+  key: process.env.API_KEY,
+  Promise: Promise
+});
+let GoogleMapsLoader = require('google-maps');
+GoogleMapsLoader.KEY = process.env.API_KEY;
+
 $(document).ready(function() {
-  $.ajax({
-    url: 'http://dinoipsum.herokuapp.com/api/',
-    type: 'GET',
-    data: {
-      format: 'json',
-      paragraphs: '1',
-      words: '1'
-    },
-    success: function(response) {
-      console.log('use API');
-      let hangman = new Hangman(response[0][0].toLowerCase());
-      showGame(hangman);
-      $('#game-area').show();
-      runGame(hangman);
-    },
-    error: function() {
-      $('#error-message').text('There was a problem proccessing your API.');
-    }
+  let promise = new Promise(function(resolve, reject) {
+    let xhr = new XMLHttpRequest();
+    let url = `https://maps.googleapis.com/maps/api/js?&key=${process.env.API_KEY}&callback=initMap`;
+    xhr.onload = function () {
+      if (this.status === 200) {
+        resolve(xhr.response);
+      } else {
+        reject(Error(xhr.statusText));
+      }
+    };
+    xhr.open("GET", url, true);
+    xhr.send();
   });
+
+  promise.then(function(response){
+    // let body = JSON.parse(response);
+    // const map = new google.maps.Map($('#map'), {
+    //   center: {lat: -34.397, lng: 150.644},
+    //   zoom: 1
+    // });
+    console.log("heyo");
+  });
+
+  // $.ajax({
+  //   url: 'http://maps.googleapis.com/maps/api/js?',
+  //   type: 'GET',
+  //   data: {
+  //     key: process.env.API_KEY,
+  //     callback: 'initMap'
+  //   },
+  //   success: function(response) {
+  //     let body = JSON.parse(response);
+  //     console.log(body);
+  //     // const map = new google.maps.Map($('#map'), {
+  //     //   center: {lat: -34.397, lng: 150.644},
+  //     //   zoom: 1
+  //     // });
+  //     console.log('almost');
+  //   },
+  //   error: function() {
+  //     $("#error-message").text('There was a problem proccessing your API.');
+  //     console.log('not quite');
+  //   }
+  // });
+
 
 });
