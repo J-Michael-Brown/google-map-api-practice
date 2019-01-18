f124f8cfd4ab475eb894d58ab58b4dfd894a7693
Daniel Burbach and Michael Brown <michael@>
2019-01-17 16:13:06 -0800

    load google map

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
+import { Map } from './map';
 import './styles.css';
 import $ from 'jquery';
 import 'bootstrap/dist/css/bootstrap.min.css';
+const loadGoogleMapsApi = require('load-google-maps-api');
+import 'load-google-maps-api';
 
 let googleMapsClient = require('@google/maps').createClient({
   key: process.env.API_KEY,
@@ -10,50 +13,60 @@ let GoogleMapsLoader = require('google-maps');
 GoogleMapsLoader.KEY = process.env.API_KEY;
 
 $(document).ready(function() {
-  let promise = new Promise(function(resolve, reject) {
-    let xhr = new XMLHttpRequest();
-    let url = `https://maps.googleapis.com/maps/api/js?&key=${process.env.API_KEY}&callback=initMap`;
-    xhr.onload = function () {
-      if (this.status === 200) {
-        resolve(xhr.response);
-      } else {
-        reject(Error(xhr.statusText));
-      }
-    };
-    xhr.open("GET", url, true);
-    xhr.send();
+  let mapElement = document.getElementById('map');
+  let loadPromise = Map.loadMap();
+  loadPromise.then(function(googleMaps) {
+    return Map.createMap(googleMaps, mapElement);
   });
 
-  promise.then(function(response){
-    // let body = JSON.parse(response);
-    // const map = new google.maps.Map($('#map'), {
-    //   center: {lat: -34.397, lng: 150.644},
-    //   zoom: 1
-    // });
-    console.log("heyo");
-  });
-
-  // $.ajax({
-  //   url: 'http://maps.googleapis.com/maps/api/js?',
-  //   type: 'GET',
-  //   data: {
-  //     key: process.env.API_KEY,
-  //     callback: 'initMap'
-  //   },
-  //   success: function(response) {
+  //   let promise = new Promise(function(resolve, reject) {
+  //     let xhr = new XMLHttpRequest();
+  //     // console.log(xhr.getResponseHeader());
+  //     let url = `https://maps.googleapis.com/maps/api/json?&key=${process.env.API_KEY}&callback=initMap`;
+  //     xhr.onload = function () {
+  //       if (this.status === 200) {
+  //         resolve(xhr.response);
+  //       } else {
+  //         reject(Error(xhr.statusText));
+  //       }
+  //     };
+  //     xhr.open("GET", url, true);
+  //     xhr.send();
+  //   });
+  //
+  //   promise.then(function(response){
+  //     console.log(response);
   //     let body = JSON.parse(response);
   //     console.log(body);
   //     // const map = new google.maps.Map($('#map'), {
   //     //   center: {lat: -34.397, lng: 150.644},
   //     //   zoom: 1
   //     // });
-  //     console.log('almost');
-  //   },
-  //   error: function() {
-  //     $("#error-message").text('There was a problem proccessing your API.');
-  //     console.log('not quite');
-  //   }
-  // });
+  //     console.log("heyo");
+  //   });
+
+  $.ajax({
+    url: 'https://maps.googleapis.com/maps/api/js?',
+    type: 'GET',
+    data: {
+      key: process.env.API_KEY,
+      callback: 'initMap'
+    },
+    success: function(response) {
+      console.log(response);
+      let body = JSON.parse(response);
+      console.log(body);
+      // const map = new google.maps.Map($('#map'), {
+      //   center: {lat: -34.397, lng: 150.644},
+      //   zoom: 1
+      // });
+      console.log('almost');
+    },
+    error: function() {
+      $("#error-message").text('There was a problem proccessing your API.');
+      console.log('not quite');
+    }
+  });
 
 
 });
---
+++ src/map.js
@@ -0,0 +1,23 @@
+
+const loadGoogleMapsApi = require('load-google-maps-api');
+const googleKey = process.env.API_KEY;
+
+export class Map {
+  constructor() {
+    this.markers = [];
+    console.log(this.markers);
+  }
+  static loadMap() {
+    return loadGoogleMapsApi({ key: `${googleKey}` });
+  }
+
+  static createMap(googleMaps, mapElement) {
+    return new googleMaps.Map(mapElement, {
+      center: {
+        lat: 45.5122,
+        lng: -122.6587
+      },
+      zoom: 8
+    });
+  }
+}
